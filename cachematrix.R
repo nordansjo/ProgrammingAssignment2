7caf3374c0d3cd999cf564cc1b9c7dd6cc15ced4
Niklas <niklas.nordansjo@gmail.com>
2015-01-23 14:32:27 +0100

    New Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Theese two functions creates can be used together to to calculate the invert of a matrix. if the calculation have been made
+## earlier it uses that value and because of that dosn´t have to do the calculation again and therefore saves time.
 
-## Write a short comment describing this function
+
+
+
+## MakeCacheMatrix creates a function that that makes a kind of matrix with 4 functions included. 
+## Set that sets the value of the matrix and removes the old free variable. get that grab the value of the matrix.
+## setinv that sets the inverted matrix with the solve function. getinv that grabs the inverted matrix.
+ 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x   <<- y
+    inv <<- NULL
+  }
+  get     <- function() x
+  setinv  <- function(solve) inv <<- solve
+  getinv  <- function() inv
+    
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  
 }
+  
 
 
-## Write a short comment describing this function
+
+## cache solves uses the kind of matrix created in makeChaceMatrix and applies the solve function. if the invert already 
+## have been calculated it uses the value from the free variabel inv and print a message that it uses a cached value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+    if(!is.null(inv)) {
+      message("Getting Value From Cache")
+     return (inv)
+    }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+  
 }
